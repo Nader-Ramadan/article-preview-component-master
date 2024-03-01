28eb0afecbfa7540825965176d8d289e475832cd
Nader Ramadan <108845596+Nader-Ramadan@users.noreply.github.com>
2024-03-01 04:40:29 +0200

    Finishing the project

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-function show() {
-    document.getElementsByClassName("share-tape")[0].style.display = "flex";
-    document.getElementsByClassName('share')[0].style.background = "var(--Very-Dark-Grayish-Blue)";
-    document.getElementsByTagName('svg')[0].style.fill = "var(--white)";
-    document.getElementsByClassName("info")[0].style.display = "none"
-    document.getElementsByClassName("user-photo")[0].style.display = "none"
-}
-function hide() {
-    document.getElementsByClassName("share-tape")[0].style.display = "none"
+let shareButton = document.querySelector(".share-tape");
+function share() {
+    document.querySelector(".share").classList.toggle("share-after")
+    shareButton.classList.toggle("show");
 }
